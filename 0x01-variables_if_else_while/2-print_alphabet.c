88343d2a28a85993d313c88a0b10869753afc81a
Diego Garzon <1265@holbertonschool.com>
2019-09-19 20:20:25 +0000

    Activity 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - main block
+ * Description: Show a alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char v_char;
+
+	for (v_char = 'a'; v_char <= 'z'; v_char++)
+		putchar(v_char);
+
+	return (0);
+}
