76fceb4c5babd4f136daeb32ae7a6ea63b0f698f
Diego Garzon <1265@holbertonschool.com>
2019-09-23 21:26:45 +0000

    First commit to proyect 0x02

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - 0. _putchar
+ * Description: show a Holberton with function _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,1 @@
+int _putchar (char v_char);
