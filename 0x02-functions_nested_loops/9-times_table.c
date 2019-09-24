a77d9fe3c5200b5ec5718d35718de6e9d342391b
Diego Garzon <1265@holbertonschool.com>
2019-09-24 18:16:10 +0000

    fix activity 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,22 +14,27 @@ void times_table(void)
 			prod = f0 * f1;
 			if (prod > 9)
 			{
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+				_putchar ((prod / 10) + '0');
+				_putchar ((prod % 10) + '0');
+			}
+			else if (prod != 0)
+			{
+				_putchar (' ');
+				_putchar (prod + '0');
 			}
 			else
 			{
-				_putchar(prod + '0');
+				_putchar (prod + '0');
 			}
 
 			if (f1 < 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar (',');
+				_putchar (' ');
 			}
 		}
 
-		_putchar('\n');
+		_putchar ('\n');
 
 	}
 }
