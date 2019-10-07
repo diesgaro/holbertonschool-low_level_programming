295bbdfcd832e0fd6d989a2c635abc808f722b5b
Diego Garzon <1265@holbertonschool.com>
2019-10-07 18:53:25 +0000

    activity 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strchr - Function that locates a character in a string
+ * @s: Pointer type char
+ * @c: Variable type char
+ * Return: A pointer s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return ('\0');
+}
