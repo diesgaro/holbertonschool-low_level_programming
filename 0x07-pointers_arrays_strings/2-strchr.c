1aab5756b66f038e30fd6dd9144ff9c6194116e8
Diego Garzon <1265@holbertonschool.com>
2019-10-07 20:17:14 +0000

    fix activity 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * _strchr - Function that locates a character in a string
  * @s: Pointer type char
@@ -10,13 +11,9 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
-		else if (*(s + 1) == c)
-		{
+		else if ((*s + 1) == c)
 			return (s + 1);
-		}
 		s++;
 	}
 	return ('\0');
