8e4cadfd3e2fd811dd8945acb0ed79394f970e5f
Diego Garzon <1265@holbertonschool.com>
2019-10-15 23:23:47 +0000

    Activity 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - Function that returns a pointer to a bidimensional array
+ * @width: Variable type int
+ * @height: Variable type int
+ * Return: A pointer.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int i, x;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		a = malloc(sizeof(int) * height);
+
+		if (a == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < height; i++)
+			{
+				a[i] = malloc(sizeof(int) * width);
+				if (a[i] == NULL)
+					return (NULL);
+
+				for (x = 0; x < width; x++)
+					a[i][x] = 0;
+			}
+			return (a);
+		}
+	}
+}
