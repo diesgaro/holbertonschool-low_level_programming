2ab8c4e743ea963f5961829fe86cfc0c75870b15
Diego Garzon <1265@holbertonschool.com>
2019-10-17 20:08:24 +0000

    Activity 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - Function that allocates memory with malloc
+ * @b: Variable type unsigned int
+ * Return: Always 0.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+
+	if (a == NULL)
+		return (98);
+	else
+		return (a);
+}
