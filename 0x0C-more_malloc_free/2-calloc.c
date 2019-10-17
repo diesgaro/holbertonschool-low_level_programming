7c3c9820147549bffb635b4b60d9f116c44d6b59
Diego Garzon <1265@holbertonschool.com>
2019-10-17 21:13:49 +0000

    Activity 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _calloc - Function that allocates memory for an array with malloc.
+ * @nmemb: Variable type unsigned int
+ * @size: Variable type unsigned int
+ * Return: A pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	a = malloc(nmemb * size);
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < nmemb; i++)
+			a[i] = 0;
+
+		return (a);
+	}
+}
