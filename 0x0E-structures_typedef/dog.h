a66b21317036b2912a8af3b14a17930e4512e519
Diego Garzon <1265@holbertonschool.com>
2019-10-22 01:24:27 +0000

    First commit to proyect 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - struct type for dogs
+ * @name: Pointer type char
+ * @age: Type float
+ * @owner: Pointer type char
+ *
+ * Description: Data structure for information about dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
