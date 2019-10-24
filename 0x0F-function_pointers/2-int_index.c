3b263584a850be3a9a50a831e21bdbb9830d3681
Diego Garzon <1265@holbertonschool.com>
2019-10-24 03:32:55 +0000

    Activity 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - Function that searches for an integer
+ *
+ * @array: Pointer type array
+ * @size: Variable type int
+ * @cmp: Function pointer
+ *
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	else
+	{
+		return (-1);
+	}
+}
