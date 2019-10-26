56a6af268a243590da244a3114a1ab60e7a9be02
Diego Garzon <1265@holbertonschool.com>
2019-10-26 18:36:24 +0000

    Fix atc. 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,22 +13,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list list;
 	char *v_string;
 
-	if (n > 0)
+	va_start(list, n);
+
+	for (i = 1; i <= n; i++)
 	{
-		va_start(list, n);
-		for (i = 1; i <= n; i++)
-		{
-			v_string = va_arg(list, char*);
+		v_string = va_arg(list, char*);
 
-			if (v_string != NULL)
-				printf("%s", v_string);
-			else
-				printf("(nil)");
+		if (v_string != NULL)
+			printf("%s", v_string);
+		else
+			printf("(nil)");
 
-			if (separator != NULL && i != n)
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(list);
+		if (separator != NULL && i != n)
+			printf("%s", separator);
 	}
+
+	printf("\n");
+	va_end(list);
 }
