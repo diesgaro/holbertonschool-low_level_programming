787b33ac62226cd76125af5ae2263ffb7afe7fd6
Diego Garzon <digarodev@gmail.com>
2019-11-01 00:30:06 +0000

    Fix activity 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,15 +8,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *current_node, *next_node;
+	list_t *next_node;
 
-	current_node = head;
-
-	while (current_node != NULL)
+	while (head != NULL)
 	{
-		next_node = current_node->next;
-		free(current_node->str);
-		free(current_node);
-		current_node = next_node;
+		next_node = head->next;
+		free(head->str);
+		free(head);
+		head = next_node;
 	}
 }
