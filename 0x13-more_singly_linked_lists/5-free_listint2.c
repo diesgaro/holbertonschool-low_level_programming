916b2baa2d7b6e32f882750445ad7f969bc2aa6b
Diego Garzon <1265@holbertonschool.com>
2019-11-05 23:12:15 +0000

    Fix act. 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,15 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current_node;
 
-	if (*head == NULL)
-		return;
-
-	while (*head != NULL)
+	if (*head != NULL)
 	{
-		current_node = *head;
-		*head = (*head)->next;
-		free(current_node);
+		while (*head != NULL)
+		{
+			current_node = *head;
+			*head = (*head)->next;
+			free(current_node);
+		}
+		*head = NULL;
 	}
-
-	*head = NULL;
 }
