c3d1004a0faeaa85588498bcf2789f6395606a17
Diego Garzon <digarodev@gmail.com>
2019-11-08 20:13:38 +0000

    Activity 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * flip_bits - Function that returns the count of number of bits to need
+ * to flip to get from one
+ *
+ * @n: Variable unsigned long int
+ * @m: Variable unsigned long int
+ *
+ * Return: A uint with the number os the bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0, comparate = 0;
+
+	comparate = n ^ m;
+
+	while (comparate != '\0')
+	{
+		count++;
+		comparate = comparate & (comparate - 1);
+	}
+
+	return (count);
+}
