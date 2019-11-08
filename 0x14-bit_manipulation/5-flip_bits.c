66fc7d311d76ed27358c6c9ba23a46f8ab72871c
Diego Garzon <digarodev@gmail.com>
2019-11-08 20:38:00 +0000

    Fix Activity 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,8 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int count = 0, comparate = 0;
+	unsigned int count = 0;
+	unsigned long int comparate = 0;
 
 	comparate = n ^ m;
 
