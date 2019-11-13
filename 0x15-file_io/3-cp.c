5757dcc83fce4aee7449988026bf0982ca112f37
Diego Garzon <digarodev@gmail.com>
2019-11-13 18:35:59 +0000

    Fix act. 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,35 +18,27 @@ int main(int ac, char *av[])
 		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd_from = open(av[1], O_RDONLY);
-
 	if (fd_from == -1)
 		dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mod);
-
 	if (fd_to == -1)
 		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	r_counter = read(fd_from, buffer, BUFFERSIZE);
+	while ((r_counter = read(fd_from, buffer, BUFFERSIZE)))
+	{
+		if (r_counter == -1)
+			dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	if (r_counter == -1)
-		dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
-	w_counter = write(fd_to, buffer, r_counter);
-
-	if (w_counter == -1)
-		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
+		w_counter = write(fd_to, buffer, r_counter);
+		if (w_counter == -1)
+			dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
 
 	if (close(fd_from) == -1)
-	{
-		dprintf(SE, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
+		dprintf(SE, "Error: Can't close fd %d\n", fd_from), exit(100);
 	if (close(fd_to) == -1)
-	{
-		dprintf(SE, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		dprintf(SE, "Error: Can't close fd %d\n", fd_to), exit(100);
 
 	return (0);
 }
