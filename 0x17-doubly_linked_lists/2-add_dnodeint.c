56e8604cbdc5c767d11995088ef4c18854d03894
Diego Garzon <digarodev@gmail.com>
2019-12-16 01:15:20 +0000

    fix act. 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->prev = NULL;
 	new_node->n = n;
-	new_node->next = *head;
+
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
 
 	*head = new_node;
 
