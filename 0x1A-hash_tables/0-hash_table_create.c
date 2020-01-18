1a59b2cd4ecb3ddcfef0967f5380ffac65df7539
Diego Garzon <digarodev@gmail.com>
2020-01-18 21:50:44 +0000

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,17 +9,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *ht = NULL;
+	hash_table_t *ht = NULL;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-		return NULL;
+		return (NULL);
 
-	ht->array = malloc(sizeof(hash_node_t) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
-		return NULL;
+		return (NULL);
 
 	ht->size = size;
 
-	return ht;
+	return (ht);
 }
