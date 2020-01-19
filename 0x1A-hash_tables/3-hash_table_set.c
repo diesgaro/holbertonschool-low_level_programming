c2f340a64b21677a63a11f815bc89b3724910726
Diego Garzon <digarodev@gmail.com>
2020-01-19 20:33:44 +0000

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
-	index_key = key_index((unsigned char *)key, ht->size);
+	index_key = key_index((const unsigned char *)key, ht->size);
 
 	current_node = ht->array[index_key];
 
